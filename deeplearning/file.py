4e4593733ea9eb60f40b66cbd8af4ec88504ad4b
luxu <zicadopv@gmail.com>
2024-08-17 16:13:33 -0300

    rede

--- deeplearning/file.py
+++ deeplearning/file.py
@@ -23,3 +23,29 @@ df['median_house_value'] = california.target
 
 print(df.head())
 
+# Separar características e alvo
+x = df.drop('median_house_value', axis=1)
+y = df['median_house_value']
+
+# formalizar características
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(x)
+
+
+# Dividir o dataset
+# X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, )
+
+
+
+# Avaliar o modelo
+# loss = model.evaluate(X_test, y_test)
+# print(f'loss (MSE): {loss}')
+
+# Construir o modelo
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(X_train.shape(1),)),
+    Dense(64, activation='relu'),
+    Dense(1) # Saída única para regressão
+])
+
+# Compilar o modelo
